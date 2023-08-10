384d287e5c03d67a50ed47215afacace0a2f7cf8
Charles Seaton <seatonct@gmail.com>
2023-08-10 11:56:50 -0500

    added imports, queryselector, main html structure, etc., to main.js

---
+++ scripts/main.js
@@ -0,0 +1,17 @@
+import { DestinationCards } from "./Destinations.js";
+import { GuestList } from "./Guests.js";
+
+const mainContainer = document.querySelector("#container");
+
+const pageHTML = `
+<article id="destinationsSection">
+    <h2>Destinations</h2>
+    ${DestinationCards()}
+</article>
+<article id="guests">
+    <h2>Current Guests</h2>
+    ${GuestList()}
+</article>
+`;
+
+mainContainer.innerHTML = pageHTML;
