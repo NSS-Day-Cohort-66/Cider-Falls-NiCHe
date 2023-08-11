1512cd3967223d65e3c605f5109a36d961267737
Nicholas Williams <nicholaswilliams910@gmail.com>
2023-08-11 15:37:02 -0500

    added destination services click event

--- scripts/Services.js
+++ scripts/Services.js
@@ -1,7 +1,8 @@
-import { getServices, getDestinationServices } from "./database.js";
+import { getServices, getDestinationServices, getDestinations } from "./database.js";
 
 const services = getServices();
 const destinationServices = getDestinationServices();
+const destinations = getDestinations();
 
 export const destinationServicesList = (destinationObj) => {
   let serviceList = `<ul>`;
@@ -18,3 +19,59 @@ export const destinationServicesList = (destinationObj) => {
 
   return serviceList;
 };
+
+export const ParkServices = () => {
+  let parkServicesHTML = "<div>Park Services:"
+
+  for (const service of services) {
+    parkServicesHTML += `<span data-id="${service.id}"
+                        data-type="parkservice"
+                        data-name="${service.name}">
+                        ${service.name}, </span>
+                        `
+  }
+  parkServicesHTML.slice(0, -2)
+  return parkServicesHTML
+}
+
+document.addEventListener(
+  "click",
+  (clickEvent) => {
+    const itemClicked = clickEvent.target
+
+      if (itemClicked.dataset.type === "parkservice") {
+        const serviceId = itemClicked.dataset.id
+
+        let serviceDestinations = []
+
+          for (const obj of destinationServices) {
+
+            if (obj.serviceId === parseInt(serviceId)) {
+
+              for (const destination of destinations) {
+
+                  if (obj.destinationId === destination.id) {
+
+                    serviceDestinations.push(destination.name)
+                  }
+              }
+            }
+          }
+          if (serviceDestinations.length === 0) {
+            window.alert(`${itemClicked.dataset.name} is not available at this current moment`)
+          } else if (serviceDestinations.length === 1) {
+            window.alert(`${itemClicked.dataset.name} is available at ${serviceDestinations}`)
+          } else if (serviceDestinations.length === 2) {
+            window.alert(`${itemClicked.dataset.name} is available at ${serviceDestinations[0]} and ${serviceDestinations[1]}`)
+          } else {
+            const lastService = serviceDestinations[serviceDestinations.length - 1]
+            serviceDestinations.pop()
+            let htmlservices = ""
+            for (const service of serviceDestinations) {
+              htmlservices += `${service}, `
+            }
+            window.alert(`${itemClicked.dataset.name} is available at ${htmlservices}and ${lastService}`)
+          }
+      }
+  }
+)
